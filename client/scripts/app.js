1708950aae8948a54513a6d3a0ddb52112fbfec6
arsimr16 <arsimr16@g.holycross.edy>
2017-09-06 15:57:01 -0700

    finished Siskel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -68,6 +68,9 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.model.on('change', function() {
+      this.render();
+    }, this);
   },
 
   events: {
@@ -76,6 +79,7 @@ var MovieView = Backbone.View.extend({
 
   handleClick: function() {
     // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -89,6 +93,7 @@ var MoviesView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
